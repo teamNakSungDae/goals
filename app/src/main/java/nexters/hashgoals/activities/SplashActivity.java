425ef066c57b638f89fda662f9d45f235fb358b5
yoon <foryou8904@naver.com>
2017-02-19 00:31:11 +0900

    Add facebook login. & Add splash image.

--- app/src/main/java/nexters/hashgoals/activities/SplashActivity.java
+++ app/src/main/java/nexters/hashgoals/activities/SplashActivity.java
@@ -4,16 +4,61 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+
 /**
- * Created by yoon on 2017. 2. 18..
+ * Created by clsan on 2017. 2. 18..
+ * Add access token tracking for facebook by clsan on 2017. 2. 19..
  */
 
 public class SplashActivity extends Activity {
+    private CallbackManager callbackManager;
+    private AccessTokenTracker accessTokenTracker;
+    private AccessToken accessToken;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, GoalActivity.class);
+        callbackManager = CallbackManager.Factory.create();
+
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(
+                    AccessToken oldAccessToken,
+                    AccessToken currentAccessToken) {
+                // Set the access token using
+                // currentAccessToken when it's loaded or set.
+                // TODO(clsan) : Access token이 만료되었을 때 어떻게 관리해줘야 하는지 리서치 해볼 것
+                AccessToken.setCurrentAccessToken(currentAccessToken);
+            }
+        };
+        // If the access token is available already assign it.
+        accessToken = AccessToken.getCurrentAccessToken();
+
+//        if (accessToken == null) {
+        startActivity(FacebookLoginActivity.class);
+//        } else {
+//            startActivity(GoalActivity.class);
+//        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        accessTokenTracker.stopTracking();
+    }
+
+    private void startActivity(Class T) {
+        Intent intent = new Intent(this, T);
         startActivity(intent);
         finish();
     }
